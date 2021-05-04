81276d0f237af4177dea9ab0110b624c76e237ce
Pat Lawrence <75762456+patlaw69@users.noreply.github.com>
2021-05-03 22:46:23 -0500

    updating working files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -16,9 +16,9 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   mars_data = Scraping.scrape_all()
+   mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return redirect("/", code=302)
    
 if __name__ == "__main__":
-      app.run()
+      app.run(debug = True)
